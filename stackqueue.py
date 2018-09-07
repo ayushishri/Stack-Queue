0bbc9d7656816c39d5102da37cc8a23988b871d6
ayushishri <32212671+ayushishri@users.noreply.github.com>
2018-09-06 22:42:38 -0400

    1

--- stackqueue.py
+++ stackqueue.py
@@ -1,1 +1,42 @@
+class stack:
+	def __init__(self):
+		self.store=[]
+		self.count=0
 
+	def push(self,val):
+		self.store+=[val]
+		self.count+=1
+		return 0
+
+	def pop(self):
+		rval=self.store[len(self.store)-1]
+		self.store=self.store[:len(self.store)-1]
+		self.count-=1
+		return rval
+
+	def isEmpty(self):
+		if (self.count==0):
+			return True
+		return False
+
+
+class queue:
+	def __init__(self):
+		self.store=[]
+		self.count=0
+
+	def enQ(self,val):
+		self.store+=[val]
+		self.count+=1
+		return 0
+
+	def deQ(self):
+		rval=self.store[0]
+		self.store=self.store[1:]
+		self.count-=1
+		return [True,rval]
+		
+	def isEmpty(self):
+		if (self.count==0):
+			return True
+		return False
